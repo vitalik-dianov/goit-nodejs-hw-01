f24d13fbb087cc0a51bf4b2d75b7a93066d11757
Vitaliy Dianov <vitalik.dianv@gmail.com>
2022-11-03 16:21:16 +0200

    add CLI app

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# GoIT-Node.js-hw-01 
+
+
+
+##  Get list
+```sh
+node index.js --action="list"  
+```
+![App Screenshot](https://i.ibb.co/YQqYGkG/list.jpg)
+
+
+## Get by id
+```sh
+node index.js --action="get" --id=5
+```
+![App Screenshot](https://i.ibb.co/2js7kgF/getById.jpg)
+
+
+## Add conctact
+```sh
+node index.js --action="add" --name="Mango" --email="mango@gmail.com" --phone="322-22-22"
+```
+![App Screenshot](https://i.ibb.co/1R21XRZ/add.jpg)
+
+
+## Remove by id
+```sh
+node index.js --action="remove" --id=3
+```
+![App Screenshot](https://i.ibb.co/K6kcp8Z/remove.jpg)
---
+++ contacts.js
@@ -0,0 +1,58 @@
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs').promises;
+
+const path = require('path');
+const contactPath = path.resolve('./db/contacts.json');
+
+async function listContacts() {
+  try {
+    const result = JSON.parse(await fs.readFile(contactPath, 'utf8'));
+    console.table(result);
+    return;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const data = JSON.parse(await fs.readFile(contactPath, 'utf8'));
+    const result = data.find(contact => contact.id !== contactId);
+    console.table(result);
+    return;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = JSON.parse(await fs.readFile(contactPath, 'utf8'));
+    const newData = data.filter(contact => contact.id !== contactId);
+    console.log(`Removed contact id - '${contactId}'`);
+    console.table(newData);
+    fs.writeFile(contactPath, JSON.stringify(newData));
+    return;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = JSON.parse(await fs.readFile(contactPath, 'utf8'));
+    const newData = [...data, { id: uuidv4(), name, email, phone }];
+    console.table(newData);
+    fs.writeFile(contactPath, JSON.stringify(newData));
+    return;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
